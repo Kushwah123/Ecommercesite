afdad4c93f0754c2b2dbb9c66ecdd6be89e4898f
DHARMENDRA KUMAR <dharmendrakumar1208@gmail.com>
2023-08-11 23:17:57 +0530

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,31 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, NotFound } from './Components/default';
+import Header from './Components/Header/Header';
+import DetailView from './Components/ItemDetails/DetailView';
+import TemplateProvider from './templates/TemplateProvider';
+import ContextProvider from './context/ContextProvider';
+import Cart from './Components/Cart/Cart';
+import { Box } from '@material-ui/core'
+
+function App() {
+  return (
+    <TemplateProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Box style={{marginTop: 54}}>
+            <Routes>
+              <Route exact path= '/' element={<Home/>} />
+              <Route exact path= '/cart' element={<Cart/>} />
+              {/* <Route exact path= '/product/:id' element={Product} /> */}
+              <Route exact path= '/product/:id' element={<DetailView/>} />
+              <Route element={NotFound} />
+            </Routes>
+          </Box>
+        </BrowserRouter>
+      </ContextProvider>
+    </TemplateProvider>
+  );
+}
+
+export default App;
